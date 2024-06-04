137c9e058f8a00f04aee4337de9291a36386ea1d
Kirill Zhumarin <kirill.zhumarin@gmail.com>
2024-06-04 20:58:46 +0000

    update

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -e
 set -x
-git pull
-npm install
-pm2 reload swilib-tools
+git fetch origin
+git diff main origin/main
+
+echo ""
+read -p "Deploy this changes? [y/n] "
+echo ""
+
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	git merge origin/main
+	npm install
+	pm2 reload swilib-tools
+fi
