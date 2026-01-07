bd6fc1f33ee6c117529e2c56398925129e729e87
Kirill Zhumarin <kirill.zhumarin@gmail.com>
2026-01-07 10:49:11 +0000

    namespace

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,16 @@ set -e
 set -x
 cd "$(dirname "$0")"
 
+PREV_MD5=$(md5sum "$0" | awk '{ print $1 }')
 git pull
+NEXT_MD5=$(md5sum "$0" | awk '{ print $1 }')
+
+if [[ "$PREV_MD5" != "$NEXT_MD5" ]]; then
+  rm -f "$LOCK_FILE"
+  echo "deploy.sh is changed, restarting..."
+  exec "$0" "$@"
+fi
+
 pnpm install --frozen-lockfile
 pnpm build
 pm2 delete swilib-tools
